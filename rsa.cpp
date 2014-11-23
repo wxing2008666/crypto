a6f85ccf8c6fcdd016ae7b585d793f1403a679a1
boddicheg <boddicheg@gmail.com>
2014-11-23 14:27:33 -0500

    generate keys done

--- rsa.cpp
+++ rsa.cpp
@@ -3,27 +3,43 @@
 #include <random>
 #include <chrono>
 
-// #define DEBUG
-const int LEFTBOARD = 1000;
-const int RIGHTBOARD = 1000000;
+#define DEBUG
+#define DEBUG_PRIME
+const ull LEFTBOARD = 1000;
+const ull RIGHTBOARD = 1000000000;
+// const ull RIGHTBOARD = 1000000;
 
 std::string rsa::encode(const std::string& input) {
-	// test is_prime
 
-	#ifdef DEBUG
-		for (ul i = 0; i < 100; ++i) {
-			cout << "Digit " << i << " : is_prime = " << is_prime(i) << endl;
-		}
-
+	#ifdef DEBUG_PRIME
 		// generate prime tests
 		for (int i = 0; i < 100; ++i) {
 			time_t start = time(NULL); 
 			ul prime = generate_prime();
 			time_t end = time(NULL);
-			cout << "Prime : " << prime << "; Time(s) : " << end - start << endl;
+			cout << "Prime : " << prime << "; Time : " << end - start << " sec" << endl;
 		}
 	#endif
+
+	// Generate all keys
+	generate_keys();
+
 	return input;
+}
+
+void rsa::generate_keys() {
+	cout << "Generate p and q..."
+	m_p = generate_prime();
+	m_q = generate_prime();
+
+	cout << "p = " << m_p << ", q = " << m_q << endl;
+	m_module = module(m_p, m_q);
+	cout << "Module: " << m_module << endl;
+	m_euler = euler_function(m_p, m_q);
+	cout << "Euler function: " << m_euler << endl;
+	m_d_key = secret_exponent(m_e_key, m_euler);
+	cout << "Secret exponent: " << m_d_key << endl;
+	cout << "Public exponent: " << m_e_key << endl;
 }
 
 std::string rsa::decode(const std::string& input) {
@@ -48,7 +64,7 @@ ull rsa::euler_function(ul first_prime, ul second_prime) {
 	return (first_prime - 1) * (second_prime - 1);
 }
 
-ull rsa::modulo(ul first_prime, ul second_prime) {
+ull rsa::module(ul first_prime, ul second_prime) {
 	return first_prime * second_prime;
 }
 
@@ -65,6 +81,15 @@ ul rsa::get_random_number() {
 	std::random_device random_device;
     std::mt19937 generator(random_device());
     std::uniform_int_distribution<ul> distribution(LEFTBOARD, RIGHTBOARD);
-
     return distribution(generator);
+}
+
+ull rsa::secret_exponent(ull e_key, ull euler) {
+	ul dkey = euler / e_key;
+	while(true) {
+		if(((dkey * e_key) % euler ) == 1) 
+			break
+		else dkey++;
+	}
+	return dkey;
 }
--- rsa.h
+++ rsa.h
@@ -10,7 +10,9 @@ typedef unsigned long long ull;
 class rsa : public crypto
 {
 public:
-	rsa() {};
+	// m_e_key use Fermat number 
+	rsa() : m_e_key(65537) 
+	{};
 	~rsa() {};
 
 	virtual std::string encode(const std::string& input) override;
@@ -20,15 +22,18 @@ private:
 	/* Variables */
 	ul m_p; // 32-bit prime digit p
 	ul m_q; // 32-bit prime digit q
-
-
-
+	ull m_module; // module
+	ull m_euler; // Euler function result
+	ull m_e_key; // public exponent
+	ull m_d_key; // secret exponent
 
 	/* Methods */
 	bool is_prime(ul digit);
 	ul generate_prime();
 	ul gcd(ul first_digit, ul second_digit); 
 	ull euler_function(ul first_prime, ul second_prime);
-	ull modulo(ul first_prime, ul second_prime);
+	ull module(ul first_prime, ul second_prime);
+	ull secret_exponent(ull e_key, ull euler);
 	ul get_random_number();
+	void generate_keys();
 };
